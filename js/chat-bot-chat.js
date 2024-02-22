c2e6479fff19abc559225515a6a7da2230d3a16f
quyir6 <158233600+quyir6@users.noreply.github.com>
2024-02-21 19:22:32 -0700

    Add files via upload

---
+++ js/chat-bot-chat.js
@@ -0,0 +1,11 @@
+var coll = document.getElementsByClassName("collapsible")
+
+for (let i = 0; i, coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+
+    var content = this.nextElementSibling;
+
+    if ()
+  })
+}
